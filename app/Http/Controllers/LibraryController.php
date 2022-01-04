c1ec83c433ac55069b01302b55ee734c0a3ef6e5
Moses Kamau <moseskamau338@gmail.com>
2022-01-04 14:31:15 +0300

    library in progress

--- app/Http/Controllers/LibraryController.php
+++ app/Http/Controllers/LibraryController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Library;
 use Illuminate\Http\Request;
 
 class LibraryController extends Controller
 {
-    //
+    public function index()
+    {
+        //load all libraries in page:
+        $libs = Library::all();
+        return view('admin.libraries', compact('libs'));
+    }
 }
