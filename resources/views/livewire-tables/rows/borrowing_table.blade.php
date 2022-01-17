b57ff08d9d6169664dd09027d05da4972bcf864a
Moses Kamau <moseskamau338@gmail.com>
2022-01-17 15:59:38 +0300

    added action buttons

--- resources/views/livewire-tables/rows/borrowing_table.blade.php
+++ resources/views/livewire-tables/rows/borrowing_table.blade.php
@@ -19,3 +19,15 @@
 <x-livewire-tables::table.cell>
  <span class="text-gray-700">{{$row->created_at}}</span>
 </x-livewire-tables::table.cell>
+{{--actions--}}
+<x-livewire-tables::table.cell>
+     <div class="flex">
+        <a href="#" class="mr-5" title="Edit book">
+            <i class="far fa-edit text-gray-600"></i>
+        </a>
+
+        <a href="#" title="Delete book">
+            <i class="far fa-trash-alt text-red-600 "></i>
+        </a>
+     </div>
+</x-livewire-tables::table.cell>
